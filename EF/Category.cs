eaca72e843c00adc4ed8aeb71acd8a9f9b435b44
IMac2 <IMac2@DESKTOP-242P176>
2022-10-26 14:19:37 +0300

    26/10/2022

--- EF/Category.cs
+++ EF/Category.cs
@@ -22,6 +22,7 @@ namespace TartugaLeksikovIzrancev.EF
     
         public int IDCategory { get; set; }
         public string NameCategory { get; set; }
+        public string Image { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Product> Product { get; set; }
